b940c413ea33b2b24cf4eef89c2fe73fb508fd6a
Leo Peng <pengzimaobuaa@163.com>
2016-04-20 21:08:37 +0800

    finish three chapters

--- app/index.js
+++ app/index.js
@@ -1,7 +1,26 @@
 /**
  * Created by leo on 4/20/16.
  */
-require('./main.css');
-var component = require('./component');
+module.exports = {
+    plugins: [
+        require('babel-plugin-syntax-class-properties'),
+        require('babel-plugin-syntax-decorators'),
+        require('babel-plugin-syntax-object-rest-spread'),
 
-document.body.appendChild(component());
+        // You can pass parameters using an array syntax
+        [
+            require('babel-plugin-transform-regenerator'),
+            {
+                async: false,
+                asyncGenerators: false
+            }
+        ]
+    ]
+};
+
+import './main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
