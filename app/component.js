4bcebdcd648e19f3a680eca90d20be38b570f76f
Leo Peng <pengzimaobuaa@163.com>
2016-04-20 12:23:18 +0800

    first demo

---
+++ app/component.js
@@ -0,0 +1,10 @@
+/**
+ * Created by leo on 4/20/16.
+ */
+module.exports = function () {
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'Hello World! My Name is Leo. I am very happy. And I find it will refresh the browser.';
+
+    return element;
+}
---
+++ app/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by leo on 4/20/16.
+ */
+require('./main.css');
+var component = require('./component');
+
+document.body.appendChild(component());
