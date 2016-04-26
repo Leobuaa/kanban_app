0c912790dcc34a5c746ba83ae997211b7ab9ee81
Leo Peng <pengzimaobuaa@163.com>
2016-04-26 10:24:07 +0800

    finish some features

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'uuid';
+import Note from './Notes.jsx';
 
 export default class App extends React.Component {
-    render() {
-        return <Note />;
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        };
     }
+
+    render() {
+        const notes = this.state.notes;
+
+        return (
+            <div>
+                <button onClick={this.addNote}>+</button>
+                <Note notes={notes} onEdit={this.editNote}/>
+            </div>
+        );
+    }
+
+    addNote = () => {
+        this.setState({
+           notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New Task'
+           }])
+        });
+    };
+
+    editNote = (id, task) => {
+        if (!task.trim()) {
+            return;
+        }
+
+        const notes = this.state.notes.map(note => {
+            if (note.id == id && task) {
+                note.task = task;
+            }
+            return note;
+        });
+
+        this.setState({notes});
+    };
 }
 
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,62 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack. My name is Leo</div>;
+export default class Note extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editing: false
+        };
+    }
+
+    render() {
+        if (this.state.editing) {
+            return this.renderEdit();
+        }
+
+        return this.renderNote();
+    }
+
+    renderEdit = () => {
+        return <input type="text"
+                      ref={
+                        element => element ?
+                        element.selectionStart = this.props.task.length : null
+                      }
+                      autoFocus={true}
+                      defaultValue={this.props.task}
+                      onBlur={this.finishEdit}
+                      onKeyPress={this.checkEnter}
+        />
+    };
+
+    renderNote = () => {
+        return <div onClick={this.edit}>{this.props.task}</div>;
+    };
+
+    edit = () => {
+        this.setState({
+            editing: true
+        });
+    };
+
+    checkEnter = (e) => {
+        if (e.key === 'Enter') {
+            this.finishEdit(E);
+        }
+    };
+
+    finishEdit = (e) => {
+        const value = e.target.value;
+
+        if(this.props.onEdit) {
+            this.props.onEdit(value);
+
+            // Exit edit mode.
+            this.setState({
+                editing: false
+            });
+        }
+    };
+
+}
